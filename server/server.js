32cbce48e62a13e9772a09b4a7a786369e4eab8c
Ranking-254 <pattinnjue@gmail.com>
2026-04-28 22:00:28 +0300

    case sensitive Ptop

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const productRoutes = require('./routes/ProductRoutes');
+const productRoutes = require('./routes/productRoutes')
 require('dotenv').config();
 
 const app = express();
